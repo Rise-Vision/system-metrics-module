c28a215091417b3f6f42fd7d7c0179a5e70bd713
santiago <santiago@nul-unu.com>
2017-11-15 11:08:05 -0600

    read memory usage

---
+++ src/collecting/memory.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+/**
+ * Reads system memory usage and return it as a promise, so we can change
+ * sync/async implementations if needed.
+ * @returns {number} Memory as a 0 to 1 value that represents used
+ * memory / total memory
+ */
+function readSystemWideUsage() {
+  const total = os.totalmem()
+  const free = os.freemem()
+  const usage = (total - free) / total
+
+  return Promise.resolve(usage)
+}
+
+module.exports = {
+  readSystemWideUsage
+}
--- test/unit/collecting/collecting.js
+++ test/unit/collecting/collecting.js
@@ -1,13 +1,21 @@
 /* eslint-env mocha */
 /* eslint-disable max-statements */
-const assert = require("assert");
+const assert = require("assert")
+const memory = require("../../../src/collecting/memory")
 
 describe("Collecting", ()=>
 {
 
-  it("should read memory usage", ()=>
+  it("should read memory usage", done =>
   {
-    assert(true)
+    memory.readSystemWideUsage()
+    .then(value =>
+    {
+      assert(value > 0, "Memory usage should be greater than 0")
+      assert(value <= 1, "Memory usage should be less or equal than 1")
+
+      done()
+    })
   })
 
 })
