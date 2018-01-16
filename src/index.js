bab48326f4bf34408e22b78c074e49545c9f22af
santiagonoguez <santiago@nul-unu.com>
2018-01-16 09:52:24 -0600

    add heartbeat loop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const ipc = require("./ipc")
-const iterations = require("./iterations")
+const ipc = require("./ipc");
+const iterations = require("./iterations");
+const heartbeat = require("common-display-module/heartbeat");
+const moduleName = require("./parameters").moduleName();
 
 // main module loop
 ipc.connect()
 .then(iterations.execute)
+.then(heartbeat.startHearbeatInterval(moduleName));
